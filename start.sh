3d8d81743fab14199c1f1c8f057759a3b287a8a2
Yokub2144 <Suwat.Paomong@gmail.com>
2025-10-06 13:45:35 +0700

    new

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
-@echo off
+#!/bin/bash
+
+# ออกถ้ามีคำสั่งใดล้มเหลว
+set -e
+
+echo "Building the project..."
 dotnet build
+
+echo "Running the project..."
 dotnet run --urls "http://0.0.0.0:5000"
-pause
