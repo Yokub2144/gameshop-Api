0f363273a4aae3bf5aa9b3e9239b334f3c756aad
Yokub2144 <Suwat.Paomong@gmail.com>
2025-10-07 20:35:00 +0700

    cc

--- Program.cs
+++ Program.cs
@@ -44,14 +44,19 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+var uploadsPath = Path.Combine(app.Environment.ContentRootPath, "Uploads");
+
+if (!Directory.Exists(uploadsPath))
+{
+    Directory.CreateDirectory(uploadsPath);
+}
+
 app.UseStaticFiles(new StaticFileOptions
 {
-
-    FileProvider = new PhysicalFileProvider(
-        Path.Combine(builder.Environment.ContentRootPath, "Uploads")),
-
+    FileProvider = new PhysicalFileProvider(uploadsPath),
     RequestPath = "/Uploads"
 });
+
 app.UseCors("AllowAngular");
 
 app.MapOpenApi();
