3c58a59caf1bc5b6ba1ebd034aefedbf57d93b82
aof20022002 <153268994+aof20022002@users.noreply.github.com>
2025-10-07 13:29:48 +0700

    Add GameController to handle user retrieval by ID

---
+++ Controller/GameController.cs
@@ -0,0 +1,32 @@
+using Gameshop_Api.Data;
+using Gameshop_Api.DTOs;
+using Gameshop_Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gameshop_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GameController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public GameController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                return NotFound(new { message = "User not found" });
+
+            return Ok(user);
+        }
+
+    }
+}
