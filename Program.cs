02d04e83aac5d9439947ecee161d52cf677f2a41
Yokub2144 <Suwat.Paomong@gmail.com>
2025-10-07 20:12:01 +0700

    ss

--- Program.cs
+++ Program.cs
@@ -50,7 +50,7 @@ app.UseStaticFiles(new StaticFileOptions
     FileProvider = new PhysicalFileProvider(
         Path.Combine(builder.Environment.ContentRootPath, "Uploads")),
 
-    RequestPath = "/uploads"
+    RequestPath = "/Uploads"
 });
 app.UseCors("AllowAngular");
 
