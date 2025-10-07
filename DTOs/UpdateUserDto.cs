da46b3a964290b084b5b4ad80c414b9fe34572d9
Yokub2144 <Suwat.Paomong@gmail.com>
2025-10-08 06:31:31 +0700

    edit updatesUser

--- DTOs/UpdateUserDto.cs
+++ DTOs/UpdateUserDto.cs
@@ -7,6 +7,6 @@ namespace Gameshop_Api.DTOs
         public string? Fullname { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
-        public IFormFile? ProfileImage { get; set; }
+        public IFormFile? profile_image { get; set; }
     }
 }
